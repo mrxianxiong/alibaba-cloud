84d3ae7ac5aae38cc3bf9b88171e18aff0472b3d
xianxiong <mrxianxiong@outlook.com>
2020-11-18 17:50:57 +0800

    1.集成sentinerl 2.添加流量控制接口测试

--- alibaba-cloud-user/src/main/java/com/mrxianx/controller/TestController.java
+++ alibaba-cloud-user/src/main/java/com/mrxianx/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.mrxianx.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +18,27 @@ public class TestController {
     @Value("${remote.key}")
     private String remoteKey;
 
+    /**
+     * 动态刷新配置文件接口测试
+     * @return
+     */
     @GetMapping(value = "/refresh/remote/value")
     public String refreshRemoteValue(){
         return remoteKey;
     }
+
+
+    /**
+     * 流量控制接口测试
+     * 基于url限流
+     * @return
+     */
+    @GetMapping(value = "/hello")
+    @SentinelResource("hello")
+    public String hello() {
+        return "Hello Sentinel";
+    }
+
+
 
 }
