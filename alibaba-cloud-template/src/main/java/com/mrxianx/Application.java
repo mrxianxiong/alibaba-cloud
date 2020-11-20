cfeb856a0747b54785a659cc374134785afc67c5
xianxiong <mrxianxiong@outlook.com>
2020-11-20 10:20:58 +0800

    1.添加项目初始化模板 2.修改pom引入的格式

---
+++ alibaba-cloud-template/src/main/java/com/mrxianx/Application.java
@@ -0,0 +1,19 @@
+package com.mrxianx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author XianXiong
+ * @Date 2020/11/20
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
